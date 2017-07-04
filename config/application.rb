f6f1ba9afe1164c126fdf145ff887f8bc1472423
Gale Bonker <gbonker@andrew.cmu.edu>
2017-07-03 18:56:53 -0700

    fix precompiling and yaml errors with deployment to heroku

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,6 @@ module DoctorReviews
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
